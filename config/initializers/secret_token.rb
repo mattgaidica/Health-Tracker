48ab89027853886a2f0bd9ad3d0dc9f9bdf3284c
Matt <matt@primestudiosllc.com>
2012-04-18 03:02:47 -0700

    no config

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-HealthTracker::Application.config.secret_token = '4352345830ca2778f7511c8e1b8387e8beb55537552ad3c8e1c11a9f5274cbf252b9ce687d340140feffff3c4d62d5c23679b46895a26b9d7a7c21be7feecbad'
